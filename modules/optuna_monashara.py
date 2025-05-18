f45a43406db309c305ac3c18b7cab09800b83d2f
Gourangkumar  Monashara <108370008+Gaurang8200@users.noreply.github.com>
2025-05-18 20:31:26 +0200

    Update optuna_monashara.py

--- modules/optuna_monashara.py
+++ modules/optuna_monashara.py
@@ -27,7 +27,7 @@ def run_optuna(model,
     def objective(trial):
         bs         = trial.suggest_categorical("BS_SUGGEST", [16, 32, 64, 128])
         lr         = trial.suggest_float("LR_SUGGEST", 1e-5, 1e-2, log=True)
-        max_epochs = trial.suggest_int("EPOCHS", 2, 2)
+        max_epochs = trial.suggest_int("EPOCHS", 25, 25)
         
 
         train_dl = DataLoader(train_subset, batch_size=bs, shuffle=True)
@@ -54,7 +54,7 @@ def run_optuna(model,
             else:
                 no_improve += 1
 
-            if no_improve >= 4: #7
+            if no_improve >= 6: #7
                 raise TrialPruned()
 
         trial.set_user_attr("best_model_state", best_state)
