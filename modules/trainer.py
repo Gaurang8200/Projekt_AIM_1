12e95bf991465514f52eb72fb46f12a833e21f5e
Gourangkumar  Monashara <108370008+Gaurang8200@users.noreply.github.com>
2025-05-22 14:17:30 +0200

    Update trainer.py

--- modules/trainer.py
+++ modules/trainer.py
@@ -72,7 +72,7 @@ class Trainer(nn.Module):
         if args["profiler"]:
             prof = torch.profiler.profile(
             activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
-            schedule=torch.profiler.schedule(wait=2, warmup=3, active=2, repeat=1),
+            schedule=torch.profiler.schedule(wait=2, warmup=3, active=6, repeat=1),
             #schedule=torch.profiler.schedule(wait=0, warmup=3, active=100, repeat=1),
             on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/trainer_profile'),
             record_shapes=True,
